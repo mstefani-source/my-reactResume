b21afe8834545fd3e2e718446890d388dd0e1a35
mstefani-source <ivanow.victor@live.ru>
2023-01-21 16:06:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Fcomponent from './Fcomponent';
+import Ccomponent from './Ccomponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Fcomponent />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +21,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <footer className='App-footer'>
+         <Ccomponent />
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Ccomponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Ccomponent extends Component {
+  render() {
+	return (
+	  <div>
+		<h1>Class component</h1>
+	  </div>
+	)
+  }
+}
