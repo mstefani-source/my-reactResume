d1e72ff660a062fc52a3185ce129ceef64fd5a8e
mstefani-source <ivanow.victor@live.ru>
2023-02-05 14:26:48 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import Fcomponent from './Fcomponent';
 import Ccomponent from './Ccomponent';
+import FMainPage from './FMainPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Fcomponent />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
+    <div>
+
+      <div className="App">
+        <header className="App-header">
+          <Fcomponent />
+        </header>
+        <main className='App-main'>
+          <div>
+            <FMainPage />
+          </div>
+        </main>
         <footer className='App-footer'>
-         <Ccomponent />
+          <Ccomponent />
         </footer>
       </div>
     </div>
